c0d71b1749340ca7b48aa62094cd1706dcd6700b
chajyn <chajyn@naver.com>
2019-12-05 14:19:20 +0900

    View Class

--- src/root/View.java
+++ src/root/View.java
@@ -1,25 +1,26 @@
 package root;
 
-import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Graphics;
+import java.awt.Image;
 
-import javax.swing.JButton;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class View {
 
+	public static int SCREEN_WIDTH = 480 + 120, SCREEN_HEIGHT = 600 + 80;
 	private JFrame frame;
-	private GameBoard primary;
+	private Background backgroundPanel;
+	private Image backgroundBufferedImage;
+	private Image processBufferedImage;
+	private Image gameBoardBufferedImage;
 	private TileGrid grid;
 	private Match match;
+	private Thread matchThread;
 	
-	Graphics gc;
-	
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -33,36 +34,37 @@ public class View {
 		});
 	}
 
-	/**
-	 * Create the application.
-	 */
 	public View() {
-	
 		initialize();
-
 	}
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.setTitle("Orchard");
+		frame.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
 		frame.setResizable(false);
-		frame.setBounds(100, 100, 450, 500);
+		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		
+		backgroundBufferedImage = new ImageIcon("./img/Background2.jpg").getImage().getScaledInstance(View.SCREEN_WIDTH, View.SCREEN_HEIGHT, Image.SCALE_SMOOTH);
+		processBufferedImage = new ImageIcon("./img/ProcessGameScreen.png").getImage().getScaledInstance(View.SCREEN_WIDTH - 120, View.SCREEN_HEIGHT - 80, Image.SCALE_SMOOTH);
+		gameBoardBufferedImage = new ImageIcon("./img/Texture.png").getImage().getScaledInstance(300, 400, Image.SCALE_SMOOTH);
+		
+		backgroundPanel = new Background(backgroundBufferedImage, processBufferedImage, gameBoardBufferedImage);
 		
 		grid = new TileGrid();
-		primary = new GameBoard(grid);
-		match = new Match(grid, primary);
-		Thread matchThread = new Thread(match);
-		
+
+		match = new Match(grid);
+		matchThread = new Thread(match);
 		matchThread.start();
 		
-		frame.getContentPane().add(primary, BorderLayout.CENTER);
+		backgroundPanel.add(grid);
+		frame.add(backgroundPanel);
+		frame.pack();
 		
-		SoundPanel newPanel = new SoundPanel();
-		frame.getContentPane().add(newPanel, BorderLayout.SOUTH);
+		Music titleMusic = new Music("ProcessGameMusic.mp3", true);
+		Thread musicThread = new Thread(titleMusic);
+		musicThread.start();
 	}
-	
 }
