91173d285a1754e04a2ec2de4bf8dad842d86d93
chajyn <chajyn@naver.com>
2019-12-05 14:17:50 +0900

    Runnable Class that Matching Check

--- src/root/Match.java
+++ src/root/Match.java
@@ -10,20 +10,18 @@ import javax.imageio.ImageIO;
 
 public class Match implements Runnable {
 	TileGrid grid;
-	GameBoard board;
 	private Random random = new Random();
 
-	public Match(TileGrid grid, GameBoard board) {
+	public Match(TileGrid grid) {
 		this.grid = grid;
-		this.board = board;
 	}
 
 	public void run() {
+		System.out.println("ㅋㅋ");
 		while(true) {
 			////match : 매치는 항상 검사
 			for (int i = 1; i <= 8; i++) {
-				for (int j = 1; j <= 8; j++) {
-
+				for (int j = 1; j <= 6; j++) {
 					// 세로줄의 검사
 					if (i != 8 && grid.GetTile(i,j).getType() == grid.GetTile(i+1, j).getType()) {
 						if (i != 1 && grid.GetTile(i,j).getType() == grid.GetTile(i-1, j).getType()) {
@@ -34,7 +32,7 @@ public class Match implements Runnable {
 					}
 
 					// 가로줄의 검사
-					if (j != 8 && grid.GetTile(i, j).getType() == grid.GetTile(i, j+1).getType()) {
+					if (j != 6 && grid.GetTile(i, j).getType() == grid.GetTile(i, j+1).getType()) {
 						if (j != 1 && grid.GetTile(i, j).getType() == grid.GetTile(i, j-1).getType()) {
 							for (int n = -1; n <= 1; n++) {
 								grid.GetTile(i, j+n).setMatch(grid.GetTile(i, j+n).getMatch()+1);
@@ -45,78 +43,58 @@ public class Match implements Runnable {
 			}
 			
 			// 애니메이팅이 종료 되었다면
-			if(board.getIsAnimating() == false) {
+			if(grid.isAnimating() == false) {
 				//second Swap : 매치 되지 않았다면 역스왑
 				int score = 0;
 				for (int i=1;i<=8;i++) {
-					for (int j=1;j<=8;j++) {
+					for (int j=1;j<=6;j++) {
 						score += grid.GetTile(i, j).getMatch();
 					}
 				}
 
-				if (board.getIsSwap() == true) {
+				if (grid.isSwap() == true) {
 					if (score == 0) {
 						int tmp;
-						tmp= grid.GetTile(board.getY0(), board.getX0()).getCol();
+						tmp= grid.GetTile(grid.getY0(), grid.getX0()).getCol();
 
 						//System.out.println("Before Col: " + tmp);
 						//System.out.println("Before Row: " + grid.GetTile(x0, y0).getRow());
-						grid.GetTile(board.getY0(), board.getX0()).setCol(grid.GetTile(board.getY(), board.getX()).getCol());
-						grid.GetTile(board.getY(), board.getX()).setCol(tmp);
+						grid.GetTile(grid.getY0(), grid.getX0()).setCol(grid.GetTile(grid.getY(), grid.getX()).getCol());
+						grid.GetTile(grid.getY(), grid.getX()).setCol(tmp);
 						//System.out.println("After Col: " + grid.GetTile(x0, y0).getCol());
 
-						tmp= grid.GetTile(board.getY0(), board.getX0()).getRow();
-						grid.GetTile(board.getY0(), board.getX0()).setRow(grid.GetTile(board.getY(), board.getX()).getRow());
-						grid.GetTile(board.getY(), board.getX()).setRow(tmp);
+						tmp= grid.GetTile(grid.getY0(), grid.getX0()).getRow();
+						grid.GetTile(grid.getY0(), grid.getX0()).setRow(grid.GetTile(grid.getY(), grid.getX()).getRow());
+						grid.GetTile(grid.getY(), grid.getX()).setRow(tmp);
 						//System.out.println("After Row: " + grid.GetTile(x0, y0).getRow());
 
-						Tile tmpTile = grid.GetTile(board.getY0(), board.getX0());
-						grid.SetTile(board.getY0(), board.getX0(), grid.GetTile(board.getY(), board.getX()));
-						grid.SetTile(board.getY(), board.getX(), tmpTile);
+						Tile tmpTile = grid.GetTile(grid.getY0(), grid.getX0());
+						grid.SetTile(grid.getY0(), grid.getX0(), grid.GetTile(grid.getY(), grid.getX()));
+						grid.SetTile(grid.getY(), grid.getX(), tmpTile);
 
 						System.out.println("매치되지 않아서 돌려보냄");
 					}
 
 				}
-				board.setIsSwap(false);
+				grid.setSwap(false);
 
 				////updateGrid
 				for (int i = 8; i > 0; i--) {
-					for (int j = 1; j <= 8; j++) {
+					for (int j = 1; j <= 6; j++) {
 						if (grid.GetTile(i, j).getMatch()>=1) {
 							for (int n = i; n > 0; n--) {
 								if (grid.GetTile(n, j).getMatch() == 0) {
-									System.out.println("스왑전 : " + grid.GetTile(n, j).getRow() + " " + grid.GetTile(n, j).getCol());
-									System.out.println("스왑전 : " + grid.GetTile(i, j).getRow() + " " + grid.GetTile(i, j).getCol());
-									int tmp;							
 
-									tmp= grid.GetTile(n,  j).getRow();
-									grid.GetTile(n, j).setRow(grid.GetTile(i, j).getRow());
-									grid.GetTile(i, j).setRow(tmp);
-
-									tmp= grid.GetTile(n,  j).getCol();
-									grid.GetTile(n, j).setCol(grid.GetTile(i, j).getCol());
-									grid.GetTile(i, j).setCol(tmp);
-
-									System.out.println("스왑후 :" + grid.GetTile(n, j).getRow() + " " + grid.GetTile(n, j).getCol());
-									System.out.println("스왑후 :" + grid.GetTile(i, j).getRow() + " " + grid.GetTile(i, j).getCol());
-
-									Tile tmpTile = grid.GetTile(n, j);
-									grid.SetTile(n, j, grid.GetTile(i, j));
-									grid.SetTile(i, j, tmpTile);
-
-									/*
-									 */
-
+									Calculator.swapTileInfo(grid.getGrid(), n, j,  i, j);
 									break;
-								} // if
-							} // for n
-						} // if
+								}
+							}
+						}
 					}
 				}
 
 
-				for (int j = 1; j <= 8; j++) {
+				for (int j = 1; j <= 6; j++) {
 					for (int i = 8, n = 0; i > 0; i--) {
 						if (grid.GetTile(i, j).getMatch()>=1) {
 							int randomNumber = random.nextInt(5) + 1;
