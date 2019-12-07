8ce2e27d9bd3f4e98283bbab6d2faf92b58a7289
eien1123 <eien2357@gmail.com>
2019-12-07 22:13:29 +0900

    OptionPanel

--- src/root/OptionPanel.java
+++ src/root/OptionPanel.java
@@ -17,16 +17,17 @@ public class OptionPanel extends JPanel
 		
 		this.img1 = img1;
 		
-		setSize(400, 400);
+		this.setBounds(0,0,600,680);
+		//setSize(600, 680);
 		setLayout(null);
 		setVisible(false);
 	}
-	
+
 	@Override
 	public void paint(Graphics g) {
 		Graphics2D g2d = (Graphics2D)g.create();
 		g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0f));
-		g2d.drawImage(img1, 100, 240, null);
+		g2d.drawImage(img1, 105, 245, null);
 		paintComponents(g);
 		this.repaint();
 	}
